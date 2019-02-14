2b2dac8de46bb1830c1d3ae4cf58f485fa37045d
Gay Fieri <45213251+mbohan96@users.noreply.github.com>
2019-02-14 14:30:13 -0500

    first update

---
+++ google-books/src/Search.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+
+
+//
+class App extends Component {
+
+this.state = {
+      books: []
+    }
+  }
+}
+
+
+
+
+render() {
+  return (
+    <div>
+     books={this.state.books}/>
+    </div>
+  );
+  }
+
+
+
+  export default App;
---
+++ google-books/src/books.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+
+<div>
+    
+    {
+      title: {
+        type: String,
+        required: true,
+      },
+
+      authors: {
+        type: String,
+      },
+
+      description: {
+        type: String,
+      },
+
+      image: {
+        type: String,
+      },
+
+      link: {
+        type: String,
+      },
+    },
+  );
+
+<div>
